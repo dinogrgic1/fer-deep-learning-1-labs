8a8050c54fa42d6203b435fef7322674d7c8e1a4
Dino Grgić <dino.grgic1@gmail.com>
2022-03-24 18:40:42 +0100

    Finished first lab.

--- lab1/mnist_shootout.py
+++ lab1/mnist_shootout.py
@@ -1,12 +1,14 @@
 import torch
 import torchvision
+import json
 from pt_deep import PTDeep
 import torch
-import numpy as np
 import matplotlib.pyplot as plt
-from data import sample_gmm_2d, class_to_onehot, eval_perf_multi, graph_surface, graph_data, metrics_print
+import time
+from data import class_to_onehot, eval_perf_multi, metrics_print
 
-dataset_root = 'data/mnist'  # change this to your preference
+dataset_root = 'dataset/'  # change this to your preference
+traindata_root = 'train_data'
 mnist_train = torchvision.datasets.MNIST(dataset_root, train=True, download=True)
 mnist_test = torchvision.datasets.MNIST(dataset_root, train=False, download=True)
 
@@ -18,15 +20,25 @@ y_train_oh = class_to_onehot(y_train)
 x_train = torch.tensor(x_train, dtype=torch.float).clone().detach().reshape(-1, 784)
 x_test = torch.tensor(x_test, dtype=torch.float).clone().detach().reshape(-1, 784)
 
-CONFIGS = [[784, 10]]
+__LAMBDA = 1e-2
+__DELTA = 0.1
+__COUNT = 3000
+
+CONFIGS = [[784, 10], [784, 100, 10]] # NO-GPU
 #CONFIGS = [[784, 10], [784, 100, 10], [784, 100, 100, 10], [784, 100, 100, 100, 10]]
 for config in CONFIGS:
     ptlr = PTDeep(config, torch.relu)
-    ptlr.train(x_train, torch.tensor(y_train_oh), 3000, 0.5, 1e-2)
-    Y = ptlr.eval(x_test.detach().numpy())
+    losses = ptlr.train(x_train, torch.tensor(y_train_oh), __COUNT, __LAMBDA, __DELTA, save_loss=True, optimizer=torch.optim.Adam)
 
-    print(Y)
-    print(y_test)
+    Y = ptlr.eval(x_test.detach().numpy())
     accuracy, pr, M = eval_perf_multi(Y, y_test)
     metrics_print(accuracy, pr, None)
+
+    config_str = ('-').join(map(str, config))
+    run = {"config": config_str, "lambda": __LAMBDA, "delta": __DELTA, "niter": __COUNT, "accuracy": accuracy, "losses": losses}
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    with open(f'{traindata_root}/{config_str}_{timestr}_data.json', 'w+') as outfile:
+        json.dump(run, outfile)
+
+
         
--- lab1/pt_deep.py
+++ lab1/pt_deep.py
@@ -20,7 +20,7 @@ class PTDeep(torch.nn.Module):
         self.weights = torch.nn.ParameterList(w_s)
         self.biases = torch.nn.ParameterList(b_s)
         self.activation = activ_function
-        
+        self.optimizer = None
 
     def forward(self, X: torch.tensor) -> torch.tensor:
         for layer in range(len(self.weights)):
@@ -33,29 +33,42 @@ class PTDeep(torch.nn.Module):
         return -torch.mean(torch.sum(torch.log(probs + 1e-13) * Yoh_, dim=1))
 
 
-    def train(self, X: torch.tensor, Yoh_: torch.tensor, param_niter: int, param_delta: float, param_lambdba = 0.) -> None:
+    def train(self, X: torch.tensor, Yoh_: torch.tensor, param_niter: int, param_delta: float, param_lambdba = 0., save_loss=False, optimizer=None) -> None:
         """Arguments:
             - X: model inputs [NxD], type: torch.Tensor
             - Yoh_: ground truth [NxC], type: torch.Tensor
             - param_niter:  number of training iterations
             - param_delta: learning rate
         """
-        optimizer = torch.optim.SGD(params=self.parameters(), lr = param_delta, weight_decay = param_lambdba)
+        losses = []
+
+        if optimizer != None:
+            optimizer = optimizer(self.parameters(), lr = param_delta, weight_decay = param_lambdba)
+        else:
+            optimizer = torch.optim.SGD(params=self.parameters(), lr = param_delta, weight_decay = param_lambdba)
         for i in range(param_niter + 1):
             loss = self.get_loss(X, Yoh_, param_lambdba)
+            if save_loss == True:
+                losses.append(str(loss.detach().numpy()))
             loss.backward()
             optimizer.step()      
 
             if i % 10 == 0:
                 print(f'step: {i}\tloss: {loss:.6f}')
             optimizer.zero_grad()
-        pass
+        return losses
             
-
     def eval(self, X: np.array) -> np.array:
         probs = self.forward(torch.tensor(X, dtype=torch.float32))
         probs = probs.detach().numpy()
         return np.argmax(probs, axis=1)
+
+    def count_params(self):
+        layer = []
+        for param in self.named_parameters():
+            layer.append(f'{param[0]}: {param[1].shape}')
+        n_parametars = sum(param.numel() for param in self.parameters())
+        return layer, n_parametars
 
 # Zadatak 5
 if __name__ == "__main__":
@@ -76,6 +89,11 @@ if __name__ == "__main__":
 
         accuracy, pr, M = eval_perf_multi(Y, Y_)
         metrics_print(accuracy, pr, M)
+        layers, num_parameters = ptlr.count_params()
+        print(f'Deep model has {num_parameters} parameters')
+        print(f'Layers of deep model are: ')
+        for layer in layers:
+            print(f'\t{layer}')
 
         graph_surface(ptlr.eval, get_box(X), 0.5, 1024, 1024)
         graph_data(torch.tensor(X), torch.tensor(Y_), torch.tensor(Y))
